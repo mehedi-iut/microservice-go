4c90d3548b5a269f186d70aa848e43174b072db7
mehedi-iut <mahedihasan@iut-dhaka.edu>
2023-01-31 17:29:20 +0600

    Added rest api get method code

--- main.go
+++ main.go
@@ -1,12 +1,51 @@
 package main
 
 import (
+	"context"
 	"log"
+	"micro-service-in-go/handlers"
+	"net/http"
 	"os"
+	"os/signal"
+	"time"
 )
 
 
 func main(){
 	l := log.New(os.Stdout, "products-api ", log.LstdFlags)
+
+	ph := handlers.NewProducts(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", ph)
+
+	s := http.Server{
+		Addr: ":9090",
+		Handler: sm,
+		ErrorLog: l,
+		ReadTimeout: 5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout: 120 * time.Second,
+	}
+
+	go func() {
+		l.Println("Starting server on port 9090")
+		err := s.ListenAndServe()
+		if err != nil{
+			l.Printf("Error starting server: %s\n", err)
+			os.Exit(1)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
+
+	sig := <-c
+	log.Println("Got signal:", sig)
+
+	// gracefully shutdown the server, waiting max 30 seconds for current operations to complete
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(ctx)
 
 }
