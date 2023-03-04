601ff27ef16fc0bc36d1cbe89ee109d60e06f56d
mehedi-iut <mahedihasan@iut-dhaka.edu>
2023-03-04 15:09:31 +0600

    added initial swagger doc

--- main.go
+++ main.go
@@ -1,3 +1,17 @@
+// Package Classification of Product API
+//
+// Documentation for Product API
+//
+// Schemes: http
+// BasePath: /
+// Version: 1.0.0
+//
+// Consumes:
+// - application/json
+//
+// Produces:
+// - application/json
+// swagger:meta
 package main
 
 import (
