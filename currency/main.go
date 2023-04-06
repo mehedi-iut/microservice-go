c64a1b2bf20f34535999bac29fc0abdc9a813bd6
mehedi-iut <mahedihasan@iut-dhaka.edu>
2023-04-06 15:49:56 +0600

    Added grpc code

---
+++ currency/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	protos "currency/currency"
+	"currency/server"
+	"github.com/hashicorp/go-hclog"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"net"
+	"os"
+)
+
+func main() {
+	log := hclog.Default()
+
+	gs := grpc.NewServer()
+	cs := server.NewCurrency(log)
+
+	protos.RegisterCurrencyServer(gs, cs)
+
+	reflection.Register(gs)
+
+	l, err := net.Listen("tcp", ":9092")
+	if err != nil {
+		log.Error("Unable to create listener", "error", err)
+		os.Exit(1)
+	}
+	gs.Serve(l)
+}
---
+++ currency/server/currency.go
@@ -0,0 +1,21 @@
+package server
+
+import (
+	"context"
+	protos "currency/currency"
+	"github.com/hashicorp/go-hclog"
+)
+
+type Currency struct {
+	log hclog.Logger
+	protos.UnimplementedCurrencyServer
+}
+
+func NewCurrency(l hclog.Logger) *Currency {
+	return &Currency{log: l}
+}
+
+func (c *Currency) GetRate(ctx context.Context, rr *protos.RateRequest) (*protos.RateResponse, error) {
+	c.log.Info("Handle GetRate", "base", rr.GetBase(), "destination", rr.GetDestination())
+	return &protos.RateResponse{Rate: 0.5}, nil
+}
