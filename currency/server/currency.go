3bc8c672eac99c04f130ea59983c4c971463650b
mehedi02 <mehedi1670@gmail.com>
2023-04-28 15:12:37 +0600

    Added basic gRPC bi-directional code

--- currency/server/currency.go
+++ currency/server/currency.go
@@ -5,6 +5,8 @@ import (
 	protos "currency/currency"
 	"currency/data"
 	"github.com/hashicorp/go-hclog"
+	"io"
+	"time"
 )
 
 type Currency struct {
@@ -26,4 +28,33 @@ func (c *Currency) GetRate(ctx context.Context, rr *protos.RateRequest) (*protos
 	}
 
 	return &protos.RateResponse{Rate: rate}, nil
+}
+
+func (c *Currency) SubscribeRates(src protos.Currency_SubscribeRatesServer) error {
+	go func() {
+		for {
+			rr, err := src.Recv()
+			// below technically not error
+			// it happens when client close the connection
+			if err == io.EOF {
+				c.log.Info("Client has closed connection")
+				break
+			}
+
+			if err != nil {
+				c.log.Error("Unable to read from client", "error", err)
+				break
+			}
+			// added inbound message
+			c.log.Info("Handle client request", "request", rr)
+		}
+	}()
+
+	for {
+		err := src.Send(&protos.RateResponse{Rate: 12.1})
+		if err != nil {
+			return err
+		}
+		time.Sleep(5 * time.Second)
+	}
 }
